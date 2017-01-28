3d228d3354b85658b7628975c2451f1e3e037121
Harrison Ianatchkov <harrison1993@gmail.com>
2017-01-28 17:43:12 -0500

    First draft toggle extension on/off

--- HAPPY/background.js
+++ HAPPY/background.js
@@ -1,6 +1,5 @@
-
-
-chrome.tabs.onUpdated.addListener(function (tabId, info) {
+var extOn = false;
+function myListener(tabId, info) {
 
     chrome.tabs.executeScript(null, {
         file : 'thirdParty/jquery-2.2.3.min.js'
@@ -17,4 +16,35 @@ chrome.tabs.onUpdated.addListener(function (tabId, info) {
             })
         })
     })
+};
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    extOn = !extOn;
+    if(extOn) {
+        chrome.browserAction.setIcon({path: "icon.png", tabId:tab.id});
+        chrome.tabs.onUpdated.addListener(myListener(tab.id, null));
+    }
+    else {
+        chrome.browserAction.setIcon({path: "iconred.png", tabId:tab.id});
+        chrome.tabs.onUpdated.removeListener(myListener(tab.id, null));
+    }
 });
+
+// chrome.tabs.onUpdated.addListener(function (tabId, info) {
+//
+//     chrome.tabs.executeScript(null, {
+//         file : 'thirdParty/jquery-2.2.3.min.js'
+//     }, function () {
+//         chrome.tabs.executeScript(null, {
+//             file: 'thirdParty/semantria/SemantriaJavaScriptSDK.js'
+//         }, function () {
+//             chrome.tabs.executeScript(null, {
+//                 file: 'profanity.js'
+//             }, function () {
+//                 chrome.tabs.executeScript(null, {
+//                     file: 'readPage.js'
+//                 })
+//             })
+//         })
+//     })
+// });
