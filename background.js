709fecaf8d5a5c35db782d80478a71a5f590f0b7
francisct <franciscotetremblay@gmail.com>
2017-01-28 12:14:50 -0500

    added listener to check for p tags on updateTab

--- background.js
+++ background.js
@@ -1,1 +1,12 @@
 
+chrome.tabs.onUpdated.addListener(function (tabId, info) {
+
+    chrome.tabs.executeScript(null, {
+        file : 'thirdParty/jquery-2.2.3.min.js'
+    }, function () {
+        chrome.tabs.executeScript(null, {
+            file : 'readPage.js'
+        });
+    });
+
+});
