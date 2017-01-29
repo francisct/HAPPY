a119a2d43932f8daa45e7e46547f52d35592f7a3
francisct <franciscotetremblay@gmail.com>
2017-01-29 12:44:27 -0500

    revert back to old popup

--- HAPPY/readPage.js
+++ HAPPY/readPage.js
@@ -22,7 +22,7 @@ $set.each(function(index){
 
     if (index == len - 1) {
             var happyPercentage = parseInt((profanityCount * 1.0 / wordCount) * 100);
-            $('#happy').html('<p style="margin-top: 12px;padding:0px;"><span id="happyPercentage" style="font: 16px Arial; display: inline;">'+happyPercentage+'% MORE HAPPY</span>  <img style="display: inline;width: 25px;margin-left: 5px;margin-bottom: 3px;" src="https://github.com/francisct/HAPPY/blob/master/HAPPY/icon-38.png?raw=true"></p>');
+            $('#happy p').html(happyPercentage + "% MORE HAPPY :D");
 
     }
 
